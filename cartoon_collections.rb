721a92eb9cc62590ce5a1cb33d295a6edb0f38e6
shu Lin <bbqlinshu@gmail.com>
2022-02-20 12:07:52 -0500

    commit tree dfe446ad740c7e47984371f3c12a6342a5d1f6a1

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves dwarves
   # Your code here
+  dwarves.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet planeteer_calls
   # Your code here
+  planeteer_calls.map do |planet|
+    planet.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls words
   # Your code here
+  words.each do |word|
+    if word.length>4
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese food
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.each do |ingredient|
+    if cheese_types.include?(ingredient) == true
+      return ingredient
+    end
+  end
+  nil
 end
